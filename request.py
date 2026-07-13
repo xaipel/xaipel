f2608552479f70644d11dedd2401e882ddf1110d
xaipel <yelisayas00@gmail.com>
2026-07-13 21:09:38 +0800

    Change DEVICE_ID in request.py
    Updated DEVICE_ID to a new value.

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ import requests
 # Configuration placeholders
 URL = "https://checkton.online/backend/spam"
 API_KEY = "lNcECAAqUrqgESlGnQeP31eFyL-S9jcHmmfDGT9zEFs"
-DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cf13d6d09f32c56eddd0d0be33-b81b-44a4-bdf7-1d02f5b97ca3"
+DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cfb1d974786d47d54c43295be4-ffba-4b9c-aa03-400d683351d8"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
