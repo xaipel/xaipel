c0699cabf20443cc4d5df40170ddcfa40b24f75c
xaipel <yelisayas00@gmail.com>
2026-07-20 21:46:35 +0800

    Update DEVICE_ID with a new value

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ import requests
 # Configuration placeholders
 URL = "https://checkton.online/backend/spam"
 API_KEY = "lNcECAAqUrqgESlGnQeP31eFyL-S9jcHmmfDGT9zEFs"
-DEVICE_ID = "kdksosososos"
+DEVICE_ID = "and_cd9e459ea708a948d5c2f5a6ca8838cfb1d974786d47d54c43295be4-ffba-4b9c-aa03-400d683351d8"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
